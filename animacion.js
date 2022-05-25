830c3569bbc9f822d0d0755d67ebf183c49cb539
valeguitart <valentinaguitart@gmail.com>
2022-05-25 02:25:36 -0300

    agregando login

---
+++ animacion.js
@@ -0,0 +1,11 @@
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(var i=0; i = animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado - 3 < scrollTop){
+            animado[i].style.opacity = 1;
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
